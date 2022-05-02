77383fcda1d4444b6f0c310d6b836af76e670639
Apple <apple@192.168.1.4>
2022-05-03 02:08:38 +0530

    requests

--- src/Config/salesbinder.php
+++ src/Config/salesbinder.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'apikey'   => '6df0f4692fd7220307f10a52308dbd16027d5314',
+    // 'apikey'   => '6df0f4692fd7220307f10a52308dbd16027d5314',
+    'apikey'   => 'e13ad8078a6d95786e532d0944094aeebc73bf13',
     'subdomain' => 'my-subdomain'
 ];
--- src/SalesBinder.php
+++ src/SalesBinder.php
@@ -19,6 +19,9 @@ class SalesBinder {
         $this->loadConfigData();
     }
     
+    /**
+     * Load ConfigSettings
+     */
     public function loadConfigData()
     {
         $config = loadConfigData();
@@ -26,25 +29,104 @@ class SalesBinder {
         $this->subdomain = array_key_exists('subdomain', $config) ? $config['subdomain'] : $this->subdomain; 
     }
     
+    /**
+     * Create Record Request
+     *
+     * @param [string] $model
+     * @param array $data
+     * @return object $response
+     */
     public function create($model,array $data)
     {
+        $response =  Http::withHeaders([
+            'Content-Type' => 'application/json',
+            ])->withBasicAuth($this->apikey, $this->secret)->post($this->createRequestUrl($model),[Str::singular($model) => $data]);
+            return $response;
+    }
+    
+    /**
+     * Edit Record Request
+     *
+     * @param string $model
+     * @param string $id
+     * @param array $data
+     * @return object $response
+     */
+    public function edit(string $model,string $id ,array $data)
+    {
+        $response =  Http::withHeaders([
+            'Content-Type' => 'application/json',
+            ])->withBasicAuth($this->apikey, $this->secret)->put($this->modelRouteUrl($model,$id),[Str::singular($model) => $data]);
+            return $response;
+    }
+
+    /**
+     * Create basic GET/POST url for request
+     *
+     * @param string $model
+     * @return void
+     */
+    private function createRequestUrl(string $model)
+    {
+        return $this->baseurl.$this->version.'/'.$model.$this->format;
+    }
+
+    /**
+     * Create get request
+     *
+     * @param string $model
+     * @param array $data
+     * @return object $response
+     */
+    public function get(string $model,array $data) {
         $createData = [
             Str::singular($model) => $data
         ];
         $createDataJson = json_encode($createData);
         $response =  Http::withHeaders([
             'Content-Type' => 'application/json',
-            ])->withBasicAuth($this->apikey, $this->secret)->post($this->createRequestUrl($model),[Str::singular($model) => $data]);
-            dd($response->body());
-    }
-        
-    public function createRequestUrl($model)
-    {
-        // https://templatebench.salesbinder.com/api/
-        return $this->baseurl.$this->version.'/'.$model.$this->format;
+            ])->withBasicAuth($this->apikey, $this->secret)->get($this->createRequestUrl($model),$data);
+            return $response;
     }
 
+    /**
+     * fetch single record by id
+     *
+     * @param string $model
+     * @param string $id
+     * @return object $response
+     */
+    public function view(string $model,string $id) {
+        $response =  Http::withHeaders([
+            'Content-Type' => 'application/json',
+            ])->withBasicAuth($this->apikey, $this->secret)->get($this->modelRouteUrl($model,$id));
+            return $response;
+    }
 
+    /**
+     * Create Request based on Model
+     *
+     * @param string $model
+     * @param string $id
+     * @return string 
+     */
+    public function modelRouteUrl(string $model, string $id) {
+        return $this->baseurl.$this->version.'/'.$model.'/'.$id.$this->format;
+    }
+
+    /**
+     * delete request
+     *
+     * @param string $model
+     * @param string $id
+     * @return void object
+     */
+    public function delete($model,string $id) {
+        $response =  Http::withHeaders([
+            'Content-Type' => 'application/json',
+            ])->withBasicAuth($this->apikey, $this->secret)->delete($this->modelRouteUrl($model,$id));
+            return $response;
+    }
 }
     
     
