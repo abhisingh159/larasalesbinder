48a48d5a0bb32c1fe0fd412aa5605f231fc4c023
Abhi Singh <abhimanevsingh@gmail.com>
2022-05-03 02:33:22 +0530

    key remove

--- src/Config/salesbinder.php
+++ src/Config/salesbinder.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    // 'apikey'   => '6df0f4692fd7220307f10a52308dbd16027d5314',
-    'apikey'   => 'e13ad8078a6d95786e532d0944094aeebc73bf13',
+    // 'apikey'   => '',
+    'apikey'   => 'api-key',
     'subdomain' => 'my-subdomain'
 ];
